34637121b620050587f367b45d92dbe884599d2b
Bryan-snw <Judithbryan_19@kharisma.ac.id>
2023-02-10 21:20:37 +0800

    perbaik about

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,9 @@ function HomePage(props) {
         <meta name="description" content="This my blog project" />
       </Head>
       <Introduction />
-      {/* <LatestProjects featuredPosts={featuredPosts} />
-      <Languange />
-      <About /> */}
+      {/* <LatestProjects featuredPosts={featuredPosts} /> */}
+      {/* <Languange /> */}
+      <About />
     </Fragment>
   );
 }
