7e68804076de510204159289db8652990acd9f6c
Bryan-snw <Judithbryan_19@kharisma.ac.id>
2023-02-10 20:48:55 +0800

    coba lagi hapus initial scale

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,7 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <Head></Head>
       <Component {...pageProps} />
     </Layout>
   );
