4442329405cfc8ab4150f0ca9412b8c0480ab93d
Bryan-snw <Judithbryan_19@kharisma.ac.id>
2023-02-10 15:00:24 +0800

    layout navigasi dan footer

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
     </Layout>
