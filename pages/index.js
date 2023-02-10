de24c9393b9f52f711a46ff2ce109f2cd0703d8a
Bryan-snw <Judithbryan_19@kharisma.ac.id>
2023-02-11 01:08:14 +0800

    laaters work fix

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ function HomePage(props) {
         <meta name="description" content="This my blog project" />
       </Head>
       <Introduction />
-      {/* <LatestProjects featuredPosts={featuredPosts} /> */}
+      <LatestProjects featuredPosts={featuredPosts} />
       <Languange />
       <About />
     </Fragment>
